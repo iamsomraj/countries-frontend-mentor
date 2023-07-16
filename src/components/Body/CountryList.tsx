80d090345a8d4751a160daf34b928590131bd65b
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-07-16 19:38:24 +0530

    👌 IMPROVE: added filtering logic

--- src/components/Body/CountryList.tsx
+++ src/components/Body/CountryList.tsx
@@ -5,11 +5,26 @@ import CountryCard from '../UI/CountryCard';
 
 const CountryList = () => {
   const {
-    state: { countries },
+    state: { countries, searchQuery, regionQuery },
   } = useContext(CountryContext);
+
+  let filteredCountries = countries;
+
+  if (searchQuery.length > 0) {
+    filteredCountries = filteredCountries.filter((country) =>
+      country.name.toLowerCase().includes(searchQuery.toLowerCase()),
+    );
+  }
+
+  if (regionQuery.length > 0) {
+    filteredCountries = filteredCountries.filter(
+      (country) => country.region.toLowerCase() === regionQuery.toLowerCase(),
+    );
+  }
+
   return (
     <div className="grid grid-cols-1 gap-8 lg:grid-cols-4">
-      {countries.map((country, index) => (
+      {filteredCountries.map((country, index) => (
         <CountryCard key={country.alpha2Code + index} country={country} />
       ))}
     </div>
