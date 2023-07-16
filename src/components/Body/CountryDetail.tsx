508a88c8a155cb30d4feef23e99fc3b8f255d388
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-07-16 20:36:53 +0530

    👌 IMPROVE: added country image in details page and info component

--- src/components/Body/CountryDetail.tsx
+++ src/components/Body/CountryDetail.tsx
@@ -3,19 +3,24 @@ import { useContext } from 'react';
 
 import { CountryContext } from '../../context/CountryContext';
 import Button from '../UI/Button';
+import CountryInfo from '../UI/CountryInfo';
 
 export default function CountryDetail() {
-  const { selectCountry } = useContext(CountryContext);
+  const {
+    state: { detailCountry: country },
+    selectCountry,
+  } = useContext(CountryContext);
 
   const onBack = () => {
     selectCountry({ country: null });
   };
 
   return (
-    <div className="flex flex-col gap-6 bg-light-mode-background px-4 py-6 transition-all duration-300 dark:bg-dark-mode-background lg:px-20 lg:py-12">
-      <div>
+    <div className="flex flex-col gap-12 bg-light-mode-background px-4 py-6 transition-all duration-300 dark:bg-dark-mode-background lg:px-20 lg:py-12">
+      <div className="flex justify-start">
         <Button withIcon text="Back" onClick={onBack} icon={ArrowLeftIcon} />
       </div>
+      {country && <CountryInfo country={country} />}
     </div>
   );
 }
--- src/components/UI/CountryInfo.tsx
+++ src/components/UI/CountryInfo.tsx
@@ -5,5 +5,52 @@ type Props = {
 };
 
 export default function CountryInfo({ country }: Props) {
-  return <div>{JSON.stringify(country)}</div>;
+  const countryFlagImage = country?.flags?.svg || country?.flag || '';
+
+  return (
+    <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
+      <div className="overflow-hidden shadow-sm">
+        <img
+          src={countryFlagImage}
+          alt={country.name}
+          className="aspect-video  overflow-hidden object-cover text-sm text-light-mode-text transition-all duration-300 group-hover:scale-110 dark:text-dark-mode-text"
+        />
+      </div>
+      <article className="flex flex-col gap-12 px-4 pb-8 pt-6">
+        <div className="flex flex-col gap-6">
+          <h3 className="text-2xl font-extrabold text-light-mode-text dark:text-dark-mode-text">
+            {country.name}
+          </h3>
+          <ul className="flex flex-col gap-1">
+            <li className="flex gap-2">
+              <span className="text-sm font-semibold text-light-mode-text dark:text-dark-mode-text">
+                Population:
+              </span>
+              <span className="text-sm text-light-mode-text dark:text-dark-mode-text/70 ">
+                {country.population.toLocaleString()}
+              </span>
+            </li>
+            <li className="flex gap-2">
+              <span className="text-sm font-semibold text-light-mode-text dark:text-dark-mode-text">
+                Region:
+              </span>
+              <span className="text-sm text-light-mode-text dark:text-dark-mode-text/70 ">
+                {country.region}
+              </span>
+            </li>
+            {country.capital && (
+              <li className="flex gap-2">
+                <span className="text-sm font-semibold text-light-mode-text dark:text-dark-mode-text">
+                  Capital:
+                </span>
+                <span className="text-sm text-light-mode-text dark:text-dark-mode-text/70 ">
+                  {country.capital}
+                </span>
+              </li>
+            )}
+          </ul>
+        </div>
+      </article>
+    </div>
+  );
 }
