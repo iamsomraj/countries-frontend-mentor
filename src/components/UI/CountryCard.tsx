dfe16a3584afd125dd563f3e46fb15a88faf2ee5
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-07-16 19:40:11 +0530

    👌 IMPROVE: added lazy loading

--- src/components/UI/CountryCard.tsx
+++ src/components/UI/CountryCard.tsx
@@ -12,6 +12,7 @@ export default function CountryCard({ country }: Props) {
         <img
           src={countryFlagImage}
           alt={country.name}
+          loading="lazy"
           className="aspect-video overflow-hidden object-cover text-sm text-light-mode-text transition-all duration-300 group-hover:scale-110 dark:text-dark-mode-text"
         />
       </div>
