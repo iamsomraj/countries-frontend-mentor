e2a61122bd9004ae011e2ddce2d56579a3cde21a
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-07-16 14:51:33 +0530

    👌 IMPROVE: added resources, setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-primary': 'hsl(209, 23%, 22%)',
+        'dark-primary-bg': 'hsl(207, 26%, 17%)',
+        'light-text': 'hsl(200, 15%, 8%)',
+        'light-input': 'hsl(0, 0%, 52%)',
+        'light-primary-bg': 'hsl(0, 0%, 98%)',
+        'light-primary': 'hsl(0, 0%, 100%)',
+      },
+    },
   },
   plugins: [],
 };
